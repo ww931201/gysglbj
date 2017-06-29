132a622769a78915958cdaac3af795e88b1a282c
wangwei <wangwei@wangwei-PC>
2017-06-29 18:04:21 +0800

    再次提交增删改查

---
+++ src/main/java/com/sinorail/gysglbj/action/CertificateAction.java
@@ -0,0 +1,98 @@
+package com.sinorail.gysglbj.action;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+import org.apache.log4j.Logger;
+
+import com.sinorail.gysglbj.extend.QuiController;
+import com.sinorail.gysglbj.model.Certificate;
+import com.sinorail.gysglbj.model.Supplier;
+
+/**
+ * 
+ *  证书的Action
+ * @author wangwei
+ *
+ */
+public class CertificateAction extends QuiController{
+	
+	private Logger log  = Logger.getLogger(CertificateAction.class);
+	
+	public void index(){
+		render("view.html");
+		log.info("***跳转到证书的展示页面");
+	}
+	
+	
+	public void detailViewSon(){
+		setAttr("certificate", Certificate.dao.queryById(getPara("id")));
+		render("detail.html");
+	}
+	
+	
+	public void addViewSon(){
+		String fatherId = getPara("fatherId");
+		setAttr("fatherId", fatherId);
+		render("save.html");
+	}
+	
+	/**
+	 * 证书保存
+	 */
+	public void save(){
+		
+		boolean status = false;
+		Certificate certificate = getModel(Certificate.class,"certificate");
+		/*certificate.setStartTime(new Timestamp(getParaToDate("START_TIME").getTime()));
+		certificate.setEndTime(new Timestamp(getParaToDate("END_TIME").getTime()));*/
+		
+		if (certificate.getId() == null) {
+			certificate.remove("ID");
+			if(!Certificate.dao.isExistZsbh(certificate.getNo())) {		
+				status = certificate.save();
+				log.info("****保存证书,编号为"+ certificate.getNo());
+			}else {
+				setAttr("content", "证书编号已存在!");
+			}
+		} else {
+			//修改
+			try {
+				if(!Certificate.dao.isExistZsbh(certificate.getNo(), certificate.getId())) {	
+					status = certificate.update();
+				}else {
+					setAttr("content", "证书编号已存在!");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		setAttr("message", status ? "保存成功!" : "保存失败!");
+		renderJson();
+	}
+	
+	/**
+	 * 证书修改
+	 */ 
+	
+	public void updateViewSon(){
+		
+		setAttr("certificate", Certificate.dao.queryById(getPara("id")));
+		render("save.html");
+		
+	}
+	/**
+	 * 证书删除
+	 * 
+	 */
+	
+	public void deleteSon(){
+		if(Certificate.dao.deleteById(getPara("id"))){
+			setAttr("status", 1);
+			log.info("****删除ID="+ getPara("id"));
+		}
+		renderJson(); 
+		
+	}
+}
